76d5b721780a25c2d3de17a2aa1ba8ee97dcdc8f
kurtreyn <kurtreyn@yahoo.com>
2023-04-17 19:27:52 -0400

    added rooms

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ROOMS } from '../../roomsInterface';
+
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
+  rooms: ROOMS[] = [];
+
+  constructor(private service: ApiService) { }
+
+  ngOnInit(): void {
+    this.loadRooms();
+  }
+
+  private loadRooms() {
+    this.service.getRooms().subscribe((rooms) => {
+      this.rooms = rooms;
+    });
+  }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, tap } from 'rxjs';
 import { USERS } from '../usersInterface';
+import { ROOMS } from '../roomsInterface';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -15,12 +16,20 @@ const httpOptions = {
 })
 export class ApiService {
   users_url: string = 'http://localhost:3000/users';
+  rooms_url: string = 'http://localhost:3000/rooms';
   form!: FormGroup;
 
   constructor(private http: HttpClient) { }
 
   signUp(user: USERS): Observable<USERS> {
     return this.http.post<USERS>(this.users_url, user, httpOptions)
+  }
+
+  getRooms(): Observable<ROOMS[]> {
+    return this.http.get<ROOMS[]>(this.rooms_url)
+      .pipe(
+        tap(room => console.log('fetched room', room))
+      )
   }
 
 }
